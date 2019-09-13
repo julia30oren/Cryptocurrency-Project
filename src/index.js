aac5acd5e0db2292d8a3f66d9e0ab56d979c4e5c
julia30oren <juliaoren30@gmail.com>
2019-09-14 01:52:05 +0300

    ready to begin...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styels.css"
+// import 'bootstrap'
+import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/alert';
+
+
 console.log('Hello world!')
+console.log('Bye!!!!!!!')
+
+jQuery(document).ready(function($){
+    $('body').css('background-color', 'red')
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styels.css',
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ]
 }
